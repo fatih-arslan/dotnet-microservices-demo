cd759428aab0340658d34c417be150bf2a1f97e9
Fatih Arslan <fatiharslan748@gmail.com>
2024-02-26 15:18:34 +0300

    create search service

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Registering the database initialization within the ApplicationStarted event
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
+});
+
+
+app.Run();
+
+// Policy for retrying HTTP requests on transient errors or NotFound (404) status codes, with indefinite retries every 3 seconds.
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
